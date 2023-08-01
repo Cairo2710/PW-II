21526d3bcbfd1cfa74553b5a03fdf386d2978dec
Cairo2710 <129015293+Cairo2710@users.noreply.github.com>
2023-08-01 19:18:45 -0300

    Add files via upload

---
+++ AULA 01/INDEX.JS
@@ -0,0 +1,8 @@
+/* DECLARANDO VARIÁVEIS */
+var nome = 'CAIO SOUSA CAIRES';
+let idade = 18;
+ const linguagem = 'JS'
+console.log("ETEC DE BARUERI - PW2 - NODEJS");
+console.log('Nome: ' + nome);
+console.log('Idade: ' + idade);
+console.log('Linguagem: ' + linguagem);
